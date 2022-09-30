c6ab9ff30cdba71c678a4131d0a54f3645d4511b
ashmar <ashotmarg2004@gmail.com>
2022-09-30 10:14:12 +0200

    first commit

--- work/fe/c0f34963d7b9dae88070d2f872a518/.command.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -ue
-echo "Registration done successfully :)" \
-
-
-cat <<-END_VERSIONS > versions.yml
-"test_gdbmvp_register:GDBMVP_REGISTER":
-    somalier: $(echo $(somalier 2>&1) | sed 's/^.*somalier version: //; s/Commands:.*$//')
-END_VERSIONS
